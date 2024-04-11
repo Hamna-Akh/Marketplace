50c3fba9ab98a4b35f2237faa0a935046dab38ed
Gilles Isaac TIESSE <gilles.tiesse@mail.mcgill.ca>
2024-04-11 14:31:52 -0400

    enum solved

--- src/main/java/com/example/Marketplace/controller/UserController.java
+++ src/main/java/com/example/Marketplace/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.Marketplace.controller;
 
 import com.example.Marketplace.model.User;
 import com.example.Marketplace.repository.UserRepository;
+import com.example.Marketplace.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,12 +19,13 @@ import java.util.List;
 public class UserController {
 
     @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
 
 
     @GetMapping("/users")
     public ResponseEntity<List<User>> getAll(){
-        List<User> users = userRepository.findAll();
+        List<User> users = userService.getAll();
+
         return ResponseEntity.status(HttpStatus.OK).body(users);
     }
 }
---
+++ src/main/java/com/example/Marketplace/service/UserService.java
@@ -0,0 +1,20 @@
+package com.example.Marketplace.service;
+
+import com.example.Marketplace.model.User;
+import com.example.Marketplace.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+
+    public List<User> getAll(){
+        return userRepository.findAll();
+
+    }
+}
