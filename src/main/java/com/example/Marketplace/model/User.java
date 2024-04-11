b94dc4414db9e9b477754d8ebe238cf59e51037e
Gilles Isaac TIESSE <gilles.tiesse@mail.mcgill.ca>
2024-04-11 15:15:44 -0400

    user login field created

--- src/main/java/com/example/Marketplace/model/User.java
+++ src/main/java/com/example/Marketplace/model/User.java
@@ -24,6 +24,6 @@ public class User {
     @Column(name = "profiletype")
     @Enumerated(EnumType.STRING)
     private ProfileType type;
-    @Column(name = "loggin")
+    @Column(name = "loggedin")
     private boolean loggedIn;
 }
