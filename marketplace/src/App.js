230613b292ae91b8f70fa29cc208149b2e649dbb
Ada Andrei <adaandrei@hotmail.com>
2024-04-12 11:54:58 -0400

    Login

--- marketplace/src/App.js
+++ marketplace/src/App.js
@@ -3,11 +3,14 @@ import './App.css';
 import DisplayAllProducts from "./Products/DisplayAllProducts";
 import DisplayProduct from "./Products/DisplayProduct";
 import CreateUser from "./User/CreateUser";
+import Login from "./User/Login";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
      <Router>
         <Routes>
+            <Route exact path="/" element={<Login />} />
             <Route exact path="/users" element={<CreateUser />} />
             <Route path="/products" element={<DisplayAllProducts />} /> {/* Use element prop instead of component */}
             <Route path="/product/{id}" element={<DisplayProduct />} /> {/* Use element prop instead of component */}
---
+++ marketplace/src/User/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            <h1>Hello, World!</h1>
+            <p>This is a template JSX file for a React component.</p>
+        </div>
+    );
+};
+
+export default Login;
