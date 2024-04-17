30e337aed9d4095c4f76088b49852f485f5a0f31
Gilles Isaac TIESSE <gilles.tiesse@mail.mcgill.ca>
2024-04-17 16:41:49 -0400

    blob issue

--- marketplace/src/Products/DisplayImage.jsx
+++ marketplace/src/Products/DisplayImage.jsx
@@ -2,20 +2,33 @@ import React, { useState, useEffect } from 'react';
 
 import axios from 'axios';
   function DisplayImage({ filename }) {
-    const [imageUrl, setImageUrl] = useState('');
+    const [imageUrl, setImageUrl] = useState(null);
 
     useEffect(() => {
-      fetchImage(filename)
+
+      fetchImage(filename);
     }, [filename]);
 
+
+    function bigEndianToLittleEndian(buffer) {
+        const length = buffer.length;
+        const result = new Uint8Array(length);
+        for (let i = 0; i < length; i++) {
+            result[i] = buffer[length - i - 1];
+        }
+        return result;
+    }
     const fetchImage = async (filename) => {
         if(filename){
             try {
-                const imageByte = await axios.get(`http://localhost:8080/image/${filename}`); // fetching the byte[]
-                const blob = new Blob([imageByte.data], {type: 'image/png'});
-                const url = URL.createObjectURL(blob);
+                const imageByte = await axios.get(`http://localhost:8080/image/${filename}`, {responseType:"blob"}); // fetching the byte[]
+//                 const bigEndianData = imageByte.data;
+//                 const littleEndianData = bigEndianToLittleEndian(bigEndianData);
+//                 const blob = new Blob([imageByte], {type: 'image/png'});
+                const url = URL.createObjectURL(imageByte.data);
                 setImageUrl(url);
-                console.log(url);
+                console.log(imageByte);
+//                 console.log(littleEndianData)
             } catch (error) {
                 console.error("error fetching image:", error);
             }
