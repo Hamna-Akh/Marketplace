44386c0605643366ffee9ea47375e8bd9f9b0a11
Hamna-Akh <hamnaakhter@hotmail.com>
2024-04-18 13:08:58 -0400

    fix test class name

--- src/test/java/com/example/Marketplace/service/MessageServiceTests.java
+++ src/test/java/com/example/Marketplace/service/MessageServiceTests.java
@@ -14,7 +14,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
-public class yesMessageServiceTests {
+public class MessageServiceTests {
 
     @InjectMocks
     MessageService messageService;
